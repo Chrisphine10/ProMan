e376c8b6ca1ac84e20ad1a378d762f68f1b82236
Chrisphine10 <ondiekchrisphine@gmail.com>
2020-06-06 04:43:20 +0300

    error corrections

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,9 @@
 
 class HomeController < ApplicationController
   def index
+	if current_user.present?
 	@projects = Project.where('team_id = ?', current_user.team_id)
 	@teams = Team.where('id = ?', current_user.team_id)
-
+	end
   end
 end
